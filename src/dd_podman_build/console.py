a2dc0c87ab3c4891a7e64f0f40fe7fd3f13b2bcf
Jordan Webb <jordan@dangerdevices.com>
2025-07-03 10:11:24 -0500

    Get color under GitHub

--- src/dd_podman_build/console.py
+++ src/dd_podman_build/console.py
@@ -1,3 +1,13 @@
+import os
+
 from rich.console import Console
 
-console = Console()
+# Not using .github.running_in_github_actions here because that file
+# imports this one. Using 132 columns under GitHub Actions as a tribute
+# to mainframe line printers, which are better than the Actions log
+# viewer in every way.
+
+console = Console(
+    force_terminal=True if "GITHUB_ACTIONS" in os.environ else None,
+    width=132 if "GITHUB_ACTIONS" in os.environ else None,
+)
